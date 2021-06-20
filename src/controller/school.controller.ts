9ad0f4e694465a7b733ffeebf38bd9acf8c96e07
unknown <ptjh0309@hanmail.net>
2021-06-20 13:12:03 +0900

    ReAdd test rest api with school data

--- src/controller/school.controller.ts
+++ src/controller/school.controller.ts
@@ -1,15 +1,41 @@
 import express from 'express';
+import { School } from '../types/school';
 
 const router = express.Router();
 
-router.get('/',(req,res) => {
-    const data = [
-        {
-            id:1,
-            name:'동북고',
-        },
-    ];
+const data: School[] = [
+    {
+        id: 1,
+        name: '동북고',
+    },
+];
+
+router.get('/', (req, res) => {
     return res.status(200).json(data);
+});
+
+router.get('/:schoolId', (req, res) => {
+    const { schoolId } = req.params;
+    if (!schoolId) {
+        return res.status(400).json();
+    }
+
+    const schoolIdNumber: number = parseInt(schoolId, 10);
+    if (!data.some(({ id }) => id === schoolIdNumber)) {
+        return res.status(404).json();
+    }
+
+    const filtered = data.filter((item: School) => item.id === schoolIdNumber);
+    return res.status(200).json(filtered[0]);
+});
+
+router.post('/', (req, res) => {
+    const school: School = req.body as School;
+    if (!school) {
+        return res.status(400).json();
+    }
+    data.push(school);
+    return res.status(201).json();
 });
 
 export default router;
